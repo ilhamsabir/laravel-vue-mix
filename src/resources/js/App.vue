3320ee6a61a7c9ca20f87a51e5ecc5c66b798dad
ilhamsabir <ilhamsabir3@gmail.com>
2020-01-07 09:52:36 +0700

    first commit

---
+++ src/resources/js/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<span>Hello 2</span>
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'app',
+}
+</script>
+
+<style lang="scss"></style>
---
+++ src/resources/js/routes/index.js
@@ -0,0 +1,39 @@
+/**
+* Vue Router
+*
+* @link http://router.vuejs.org/en/installation.html
+*/
+import Vue from 'vue';
+import VueHead from 'vue-head'
+import VueRouter from 'vue-router';
+
+// pages
+import Home from '../pages/Home.vue';
+import About from '../pages/About.vue';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+Vue.use(VueHead);
+Vue.use(VueRouter);
+
+const routes = [
+	{
+		path: '/',
+		name: 'home',
+		component: Home
+	},
+	{
+		path: '/about',
+		name: 'about',
+		component: About
+	},
+];
+
+const router = new VueRouter({
+	mode: 'history',
+	// fallback: true, //router should fallback to hash (#) mode when the browser does not support history.pushState
+	routes
+});
+
+export default router;
